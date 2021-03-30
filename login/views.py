3caed4b15cb2f750da041a076a76ce3b7f6d12d3
HITANSH DOSHI <>
2021-03-30 08:52:46 +0530

    login front end completed

--- login/views.py
+++ login/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def login(request):
+  # template = loader.get_template('login/login.html')
+  return HttpResponse(render(request, 'login/login.html'))
--- pizza_ms/urls.py
+++ pizza_ms/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
+    path('auth/', include('login.urls')),
 ]
